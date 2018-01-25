984b486a4d05024405f824b24f0a8bbc703634cc
GlebSviripa <GSviripa@tula.co>
2018-01-25 19:55:18 +0200

    update LOCATION SETTINGS

--- app/src/main/java/com/example/android/walkmehome/utils/LocationHelper.java
+++ app/src/main/java/com/example/android/walkmehome/utils/LocationHelper.java
@@ -10,8 +10,13 @@ import android.content.Context;
 import android.location.Location;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class LocationHelper {
 
@@ -19,5 +24,17 @@ public class LocationHelper {
     public static void getCurrentLocation(Context context, OnSuccessListener<Location> listener) {
         FusedLocationProviderClient mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
         mFusedLocationClient.getLastLocation().addOnSuccessListener(listener);
+    }
+
+    public static void setLocationProviderSettings(Context context) {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(60000);
+        mLocationRequest.setFastestInterval(20000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+        SettingsClient client = LocationServices.getSettingsClient(context);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
     }
 }
